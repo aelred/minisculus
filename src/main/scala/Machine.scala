dc85ac90ee555ddad4a536fce9213770b66a5a77
aelred <felix.chapman@babylonhealth.com>
2021-01-18 09:17:11 +0000

    Add some short docs

--- src/main/scala/Machine.scala
+++ src/main/scala/Machine.scala
@@ -1,17 +1,21 @@
 import Wheel.{shift, shiftPrevious}
 
+/** Encoding machine, comprises a sequence of [[Wheel]]s */
 class Machine(wheels: Wheel*):
+  /** Encode a string of characters */
   def encode(string: String): String =
     string map { char =>
       wheels.foldRight(char) { _ encode _ }
     }
 
+  /** Decode a string of characters such that `machine.decode(machine.encode(s)) == s` */
   def decode(string: String): String = decoder.encode(string)
 
+  /** A machine that can decode messages encoded by the original machine */
   def decoder: Machine = Machine(wheels.map(_.inverse).reverse: _*)
 
 
 object Machine:
   def markI(wheel: Int) = Machine(shift(wheel))
-  def markII(wheel1: Int, wheel2: Int) = Machine(shift(wheel1), shift(wheel2, -2))
-  def markIV(wheel1: Int, wheel2: Int) = Machine(shift(wheel1), shift(wheel2, -2), shiftPrevious(2))
+  def markII(wheel1: Int, wheel2: Int) = Machine(shift(wheel1), shift(wheel2 * -2))
+  def markIV(wheel1: Int, wheel2: Int) = Machine(shift(wheel1), shift(wheel2 * -2), shiftPrevious(2))
--- src/main/scala/Wheel.scala
+++ src/main/scala/Wheel.scala
@@ -2,23 +2,30 @@ import java.lang.Math.floorMod
 
 import Wheel.{keyboard, mod}
 
+/** Wheel in an encoding machine */
 trait Wheel:
+  /** Encode a [[Char]] to another [[Char]] */
   def encode(char: Char): Char =
     val index = keyboard.indexOf(char)
     if index == -1 then throw Exception(f"Character '$char' not on keyboard")
     val newIndex = mod(mapping(index))
     keyboard.charAt(newIndex)
   
-  def mapping(index: Int): Int
+  /** Defines how characters are mapped (as integers) */
+  protected def mapping(index: Int): Int
   
+  /** Invert the wheel, such that `wheel.encode(wheel.inverse.encode(c)) == c` */
   def inverse: Wheel
+
 
 object Wheel:
   
-  def shift(num: Int, scale: Int = 1): Wheel = new Wheel:
-    def mapping(index: Int) = index + num * scale
-    def inverse = shift(num, -scale)
-  
+  /** Simple shift wheel that shifts a charater a certain number of steps */
+  def shift(num: Int): Wheel = new Wheel:
+    def mapping(index: Int) = index + num
+    def inverse = shift(-num)
+
+  /** Shift wheel based on index of previous character */
   def shiftPrevious(scale: Int = 1): Wheel = new Wheel:
     private var position: Int = 0
     
@@ -29,7 +36,7 @@ object Wheel:
     
     def inverse = shiftPreviousInverse(-scale)
 
-  def shiftPreviousInverse(scale: Int = 1): Wheel = new Wheel:
+  private def shiftPreviousInverse(scale: Int = 1): Wheel = new Wheel:
     private var position: Int = 0
 
     def mapping(index: Int) =
